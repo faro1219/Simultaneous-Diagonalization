1d2199b931ea8fb1fb7308b1385487844eea0021
jaydu1 <413075930@qq.com>
2020-08-08 16:30:45 -0500

    Update nearest_normal.m

--- nearest_normal.m
+++ nearest_normal.m
@@ -1,15 +1,16 @@
 %% add path
 addpath(genpath('gloptipoly3'))
-addpath('SeDuMi_1_3')
-
+addpath(genpath('SeDuMi_1_3'));
+% addpath(genpath('SparsePOP303'));
+% addpath(genpath('YALMIP-master'));
 %% define constant matrix A
 % Example 1
 % https://link.springer.com/content/pdf/10.1007/BF01937278.pdf
 % The nearest normal matrix of A is 
 % N = [1.1449+1i*0.8324, -2.0841-1i*0.9958;
 % -1.0695 -1i*2.0473,-0.1948- 1i*0.4603]
-% A = [0.7616+1i*1.2296, -1.4740-1i*0.4577;
-%     -1.6290-1i*2.6378, 0.1885-1i*0.8575];
+A = [0.7616+1i*1.2296, -1.4740-1i*0.4577;
+    -1.6290-1i*2.6378, 0.1885-1i*0.8575];
 
 % Example 2
 % https://link.springer.com/content/pdf/10.1007/s10444-019-09717-6.pdf
@@ -19,7 +20,7 @@ addpath('SeDuMi_1_3')
 
 % Example 3
 % randomly generated
-A = randn(3,3)+1i*randn(3,3);    
+% A = randn(3,3)+1i*randn(3,3);    
 
 A_real = real(A);
 A_image= imag(A);
@@ -27,6 +28,8 @@ n = size(A,1);
 
 
 %% declare variables and solve by SDP relaxation
+% mset('yalmip',true)
+% mset(sdpsettings('solver','sparsepop'));
 mpol('X_real',n,n)
 mpol('X_image',n,n)
 LMI = 2; % order of SDP relaxation
